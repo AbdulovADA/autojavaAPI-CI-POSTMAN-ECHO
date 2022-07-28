24d7aacf95a840942e7f44ed2ae06c2c62b7f8aa
AbdulovADA <lil.vlad7608@gmail.com>
2022-07-28 17:26:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/b0bon25le7kx8egj?svg=true)](https://ci.appveyor.com/project/AbdulovADA/autojavaapi-ci-postman-echo)
---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void PostmanEchoTest() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                //.body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("data manager")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                //.body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("data man"))
+        ;
+    }
+}
